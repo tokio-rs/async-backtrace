94b96c75322b6b906cdce63212b5048b5416ef6c
John Wrenn <jack@wrenn.fyi>
2022-10-19 15:21:08 -0400

    make example more interesting

--- backtrace/examples/taskdump.rs
+++ backtrace/examples/taskdump.rs
@@ -1,12 +1,22 @@
-// This example prints something like:
-// ╼ taskdump::foo::{{closure}} at backtrace/examples/taskdump.rs:12:1
-//   └╼ taskdump::bar::{{closure}} at backtrace/examples/taskdump.rs:17:1
-//      └╼ taskdump::buz::{{closure}} at backtrace/examples/taskdump.rs:27:1
-// ╼ taskdump::baz::{{closure}} at backtrace/examples/taskdump.rs:32:1
+// This example outputs something like:
+// ╼ taskdump::foo::{{closure}} at backtrace/examples/taskdump.rs:20:1
+//   └╼ taskdump::bar::{{closure}} at backtrace/examples/taskdump.rs:25:1
+//      ├╼ taskdump::buz::{{closure}} at backtrace/examples/taskdump.rs:35:1
+//      │  └╼ taskdump::baz::{{closure}} at backtrace/examples/taskdump.rs:40:1
+//      └╼ taskdump::fiz::{{closure}} at backtrace/examples/taskdump.rs:30:1
+// ╼ taskdump::pending::{{closure}} at backtrace/examples/taskdump.rs:15:1
 
 #[tokio::main]
 async fn main() {
-    foo().await;
+    tokio::select! {
+        _ = tokio::spawn(pending()) => {}
+        _ = foo() => {}
+    };
+}
+
+#[async_backtrace::framed]
+async fn pending() {
+    std::future::pending::<()>().await
 }
 
 #[async_backtrace::framed]
@@ -26,7 +36,7 @@ async fn fiz() {
 
 #[async_backtrace::framed]
 async fn buz() {
-    println!("{}", tokio::spawn(baz()).await.unwrap());
+    println!("{}", baz().await);
 }
 
 #[async_backtrace::framed]
